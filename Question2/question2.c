93a126fdaf7554e0018615bdc8106328e00b6f01
Sitesh Roy <31314384+sitesh221b@users.noreply.github.com>
2019-04-09 04:14:43 +0530

    Create question2.c

---
+++ Question2/question2.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){   //To give the filename for input and output while compiling
+    int pipefd[2];
+    int fd;
+    pid_t p;
+    char parentBuff[100], childBuff[100];
+    int readLength;
+    
+    pipe(pipefd);
+    
+    p = fork();
+    
+    char *sourceFileName = argv[1];
+    char *destinationFileName = argv[2];
+    
+    if(p < 0){
+        printf("\nError creating the child process!");
+    }
+    else if(p > 0){
+    
+        printf("This is Parent Process\n");
+        
+        close(pipefd[0]);   //Closing reading end
+        fd = open(sourceFileName, O_RDONLY, 0777);
+        readLength = read(fd, parentBuff, 100);
+        write(pipefd[1], parentBuff, readLength);
+        close(pipefd[1]);
+    }
+    else if(p == 0){
+    
+        printf("This is Child Process\n");
+        
+        //Reading the source file
+        close(pipefd[1]);   //Closing the writing end
+        readLength = read(pipefd[0], buff, 100);
+        close(pipefd[0]);
+        
+        //Writing in destination file
+        fd = open(destinationFileName, O_CREAT | O_WRONLY, 0777);
+        write(fd, buff, readLength);
+    }
+    return 0;
+}
